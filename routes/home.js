252db505c40bc786999e1e9058fba88c278f111e
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2019-04-15 17:01:20 +0200

    check if loggedin

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,15 @@
 module.exports = (app) => {
-	app.get('/', (req, res) => {
-		res.send('wow');
+	app.get('/isloggedin', (req, res) => {
+		if (req.session.passport) {
+			res.send({
+				code: 0,
+				status: true
+			});
+		} else {
+			res.send({
+				code: 0,
+				status: false
+			});
+		}
 	});
 };
