fec15295192fb26921d4179b74b38c63114a21e3
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2019-05-07 15:38:17 +0200

    added image download

--- src/routes/cli.js
+++ src/routes/cli.js
@@ -1,7 +1,7 @@
 const { projects } = require('../methods');
-const { getSingleProject } = projects;
+const { getSingleProject, getProjectImages } = projects;
 const { models } = require('../db');
-const { Typography, Project } = models;
+const { Typography, Project, User } = models;
 const stream = require('stream');
 const fs = require('fs');
 
@@ -20,7 +20,8 @@ module.exports = (app, passport) => {
 				req.logIn(user, async (err) => {
 					if (err) return next(err);
 					const project = await Project.findByPk(projectId);
-					const projectHasUser = await project.hasUser(user);
+					const userModel = await User.findByPk(user.id);
+					const projectHasUser = await project.hasUser(userModel);
 					if (projectHasUser) {
 						const projects = await getSingleProject(user.id, projectId);
 						res.send(projects);
@@ -47,7 +48,8 @@ module.exports = (app, passport) => {
 					if (err) return next(err);
 					try {
 						const project = await Project.findByPk(projectId);
-						const projectHasUser = await project.hasUser(user);
+						const userModel = await User.findByPk(user.id);
+						const projectHasUser = await project.hasUser(userModel);
 						if (projectHasUser) {
 							const typo = await Typography.findAll({
 								raw: true,
@@ -77,6 +79,68 @@ module.exports = (app, passport) => {
 							res.set('Content-Type', 'text/plain');
 
 							readStream.pipe(res);
+						}
+					} catch (e) {
+						res.send(false);
+					}
+				});
+			} else {
+				res.status(400).json(info);
+			}
+		})(req, res, next);
+	});
+
+	/**
+	 * Single project
+	 * @type GET
+	 * @middleware isLoggedIn
+	 * @param {Int} req.user.id - User session ID
+	 * @param {Int} req.params.id - Project ID
+	 */
+	app.post('/project/:id/images/download', async (req, res, next) => {
+		passport.authenticate('local-signin', (err, user, info) => {
+			const projectId = req.body.projectId;
+			if (err) return next(err);
+			if (info && info.code === 5) {
+				res.send(info);
+				return;
+			}
+			if (user) {
+				req.logIn(user, async (err) => {
+					if (err) return next(err);
+					try {
+						const project = await Project.findByPk(projectId);
+						const userModel = await User.findByPk(user.id);
+						const projectHasUser = await project.hasUser(userModel);
+						if (projectHasUser) {
+							const projectImages = await getProjectImages(user.id, projectId);
+							if (projectImages) {
+								const images = projectImages.images.map((image) => {
+									const img = image.split('./')[1];
+									return `./${img}`;
+								});
+								let zip = new AdmZip();
+								images.forEach((image) => {
+									zip.addLocalFile(image);
+								});
+
+								var willSendthis = zip.toBuffer();
+								// res.download(willSendthis);
+								// var fileContents = Buffer.from(fileData, "base64");
+
+								var readStream = new stream.PassThrough();
+								readStream.end(willSendthis);
+
+								res.set('Content-disposition', 'attachment; filename=' + projectId);
+								res.set('Content-Type', 'text/plain');
+
+								readStream.pipe(res);
+							} else {
+								res.status(404).json({
+									code: 3,
+									message: 'No project found with this ID'
+								});
+							}
 						}
 					} catch (e) {
 						res.send(false);
