0f610b18acb9605e34ea6b4463569b7c0969352c
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2019-05-09 12:00:35 +0200

    removed sutpid session bug and also update on cli

--- src/routes/cli.js
+++ src/routes/cli.js
@@ -20,13 +20,23 @@ module.exports = (app, passport) => {
 				req.logIn(user, async (err) => {
 					if (err) return next(err);
 					const project = await Project.findByPk(projectId);
-					const userModel = await User.findByPk(user.id);
-					const projectHasUser = await project.hasUser(userModel);
-					if (projectHasUser) {
-						const projects = await getSingleProject(user.id, projectId);
-						res.send(projects);
+					if (project) {
+						const userModel = await User.findByPk(user.id);
+						const projectHasUser = await project.hasUser(userModel);
+						if (projectHasUser) {
+							const projects = await getSingleProject(user.id, projectId);
+							req.session.destroy(() => {
+								res.send(projects);
+							});
+						} else {
+							req.session.destroy(() => {
+								res.send(false);
+							});
+						}
 					} else {
-						res.send(false);
+						req.session.destroy(() => {
+							res.send(false);
+						});
 					}
 				});
 			} else {
@@ -48,37 +58,42 @@ module.exports = (app, passport) => {
 					if (err) return next(err);
 					try {
 						const project = await Project.findByPk(projectId);
-						const userModel = await User.findByPk(user.id);
-						const projectHasUser = await project.hasUser(userModel);
-						if (projectHasUser) {
-							const typo = await Typography.findAll({
-								raw: true,
-								where: {
-									projectId
-								}
-							});
-
-							const allfontFamilies = [...new Set(typo.map((font) => font.family))];
-							let zip = new AdmZip();
-							allfontFamilies.forEach((font) => {
-								const folder = `./uploads/projects/${projectId}/fonts/${font.toLowerCase()}`;
-
-								const files = fs.readdirSync(folder);
-								files.forEach((file) => {
-									zip.addLocalFile(`${folder}/${file}`);
+						if (project) {
+							const userModel = await User.findByPk(user.id);
+							const projectHasUser = await project.hasUser(userModel);
+							if (projectHasUser) {
+								const typo = await Typography.findAll({
+									raw: true,
+									where: {
+										projectId
+									}
 								});
-							});
-							var willSendthis = zip.toBuffer();
-							// res.download(willSendthis);
-							// var fileContents = Buffer.from(fileData, "base64");
 
-							var readStream = new stream.PassThrough();
-							readStream.end(willSendthis);
+								const allfontFamilies = [...new Set(typo.map((font) => font.family))];
+								let zip = new AdmZip();
+								allfontFamilies.forEach((font) => {
+									const folder = `./uploads/projects/${projectId}/fonts/${font.toLowerCase()}`;
 
-							res.set('Content-disposition', 'attachment; filename=' + projectId);
-							res.set('Content-Type', 'text/plain');
+									const files = fs.readdirSync(folder);
+									files.forEach((file) => {
+										zip.addLocalFile(`${folder}/${file}`);
+									});
+								});
+								var willSendthis = zip.toBuffer();
+								// res.download(willSendthis);
+								// var fileContents = Buffer.from(fileData, "base64");
 
-							readStream.pipe(res);
+								var readStream = new stream.PassThrough();
+								readStream.end(willSendthis);
+
+								res.set('Content-disposition', 'attachment; filename=' + projectId);
+								res.set('Content-Type', 'text/plain');
+
+								readStream.pipe(res);
+								req.session.destroy();
+							}
+						} else {
+							res.send(false);
 						}
 					} catch (e) {
 						res.send(false);
@@ -110,37 +125,41 @@ module.exports = (app, passport) => {
 					if (err) return next(err);
 					try {
 						const project = await Project.findByPk(projectId);
-						const userModel = await User.findByPk(user.id);
-						const projectHasUser = await project.hasUser(userModel);
-						if (projectHasUser) {
-							const projectImages = await getProjectImages(user.id, projectId);
-							if (projectImages) {
-								const images = projectImages.images.map((image) => {
-									const img = image.split('./')[1];
-									return `./${img}`;
-								});
-								let zip = new AdmZip();
-								images.forEach((image) => {
-									zip.addLocalFile(image);
-								});
+						if (project) {
+							const userModel = await User.findByPk(user.id);
+							const projectHasUser = await project.hasUser(userModel);
+							if (projectHasUser) {
+								const projectImages = await getProjectImages(user.id, projectId);
+								if (projectImages) {
+									const images = projectImages.images.map((image) => {
+										const img = image.split('./')[1];
+										return `./${img}`;
+									});
+									let zip = new AdmZip();
+									images.forEach((image) => {
+										zip.addLocalFile(image);
+									});
+									var willSendthis = zip.toBuffer();
+									// res.download(willSendthis);
+									// var fileContents = Buffer.from(fileData, "base64");
 
-								var willSendthis = zip.toBuffer();
-								// res.download(willSendthis);
-								// var fileContents = Buffer.from(fileData, "base64");
+									var readStream = new stream.PassThrough();
+									readStream.end(willSendthis);
 
-								var readStream = new stream.PassThrough();
-								readStream.end(willSendthis);
+									res.set('Content-disposition', 'attachment; filename=' + projectId);
+									res.set('Content-Type', 'text/plain');
 
-								res.set('Content-disposition', 'attachment; filename=' + projectId);
-								res.set('Content-Type', 'text/plain');
-
-								readStream.pipe(res);
-							} else {
-								res.status(404).json({
-									code: 3,
-									message: 'No project found with this ID'
-								});
+									readStream.pipe(res);
+									req.session.destroy();
+								} else {
+									res.status(404).json({
+										code: 3,
+										message: 'No project found with this ID'
+									});
+								}
 							}
+						} else {
+							res.send(false);
 						}
 					} catch (e) {
 						res.send(false);
